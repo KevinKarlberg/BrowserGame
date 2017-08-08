6c7516e7564789c6bc3dad2bd5e6214f44c6dccd
Kevin Karlberg <kevinkarlberg@hotmail.se>
2017-08-08 14:06:35 +0200

    Added most of the shell for the new project

---
+++ ClassLibrary1/Game.AdminWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Game.AdminWebsite.Startup))]
+namespace Game.AdminWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
